581fd92dd3d2f5e0d6141102664b1556a08edf16
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-30 23:18:19 -0400

    challench succesfull

--- app.js
+++ app.js
@@ -6,6 +6,13 @@
 // .open-modal
 
 const modalBtn = document.querySelector('.modal-btn');
-const modalOverlay = document.querySelectoru('.modal-overlay');
-const closeBtn = document.querySelectoru('.close-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
 
+modalBtn.addEventListener('click', function() {
+    modalOverlay.classList.add('open-modal')
+})
+
+closeBtn.addEventListener('click', function() {
+    modalOverlay.classList.remove('open-modal')
+})
